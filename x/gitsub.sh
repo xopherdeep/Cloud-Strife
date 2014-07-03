7c4736831478a7ee395e8c166ac76b94aa3c480f
MidKnight Knerd <xopherdeep@users.noreply.github.com>
2014-07-03 01:39:08 -0700

    Update gitsub.sh

--- x/gitsub.sh
+++ x/gitsub.sh
@@ -11,12 +11,15 @@ for item in ${array[*]}
 do
 
     printf "Adding   %s\n" $item
+    git rm x/$item -r
     git submodule add https://github.com/XenGenie/$item x/$item 
-    
+
     printf "Init Modules   %s\n" $item
     cd x/
 
     cd $item/
+
+    git pull
     ./gitsub.sh
     cd ../../
 done
