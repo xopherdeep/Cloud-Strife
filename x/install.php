0a22bd570c7bd05d09d578fe86a5daca19d96daa
xopherdeep <heylisten@xtiv.net>
2014-07-17 13:15:24 -0700

    updating install

--- x/install.php
+++ x/install.php
@@ -27,8 +27,7 @@
     // mysys("cd ../../");
 
 
-    exit;
-
+    exit; 
     //header("Location: /");
   } 
 ?>
@@ -333,7 +332,7 @@ body {
               <div class="carousel-caption">
                 <h1><?php echo $value ?></h1>
                 <p><?php echo get_data("https://raw.githubusercontent.com/XenGenie/$value/master/README.md")?></p>
-                <p><a class="btn btn-lg btn-primary" onClick="return popup(this, 'notes')" href="./install.php?suite=<?php echo $value; ?>" role="button">Install</a></p>
+                <p><a class="btn btn-lg btn-primary" onClick="return popup(this, 'notes')" href="/x/install.php?suite=<?php echo $value; ?>" role="button">Install</a></p>
               </div>
             </div>
           </div>
